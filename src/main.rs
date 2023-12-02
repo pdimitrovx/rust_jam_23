4756ab683f985e21b0fc093c1568c961d25da35c
eL_bArTo <bar_el_barto@protonmail.com>
2023-12-02 00:06:42 +0000

    add santa :D

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
-use macroquad::prelude::*;
+use macroquad::{prelude::*, miniquad::conf::Platform};
 
-#[macroquad::main("BasicShapes")]
+fn window_conf() -> Conf {
+    Conf {
+        window_title: String::from("Ho ho Ho-ld my bear!"),
+        window_width: 640,
+        window_height: 480,
+        window_resizable: false,
+        high_dpi: false,
+        fullscreen: false,
+        sample_count: 1,
+        icon: None,
+        platform: Platform::default(),
+    }
+}
+
+#[macroquad::main(window_conf)]
 async fn main() {
+    let santa_texture: Texture2D = load_texture("res/santa.png").await.unwrap();
+
     loop {
-        clear_background(LIGHTGRAY);
+        clear_background(WHITE);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        if is_key_down(KeyCode::Escape) {
+            break;
+        }
 
-        draw_text("HELLO", 20.0, 20.0, 30.0, DARKGRAY);
+        draw_texture(&santa_texture, 0., 0., WHITE);
 
         next_frame().await
     }
