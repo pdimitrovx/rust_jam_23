2d923496596c389c6a23d6a5361cbab43a2cf062
eL_bArTo <bar_el_barto@protonmail.com>
2023-12-05 22:06:42 +0000

    fix resolution

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,9 @@ fn window_conf() -> Conf {
 async fn main() {
 
     init_resources().await;
-    let game_render_target = render_target(GAME_SIZE_X, GAME_SIZE_Y);
+
+    let mut camera =
+            Camera2D::from_display_rect(Rect::new(0.0, 0.0, GAME_SIZE_X as f32, GAME_SIZE_Y as f32));
 
     let mut world = World::new();
     let mut game = Game::new(&mut world);
@@ -35,54 +37,37 @@ async fn main() {
             break;
         }
         
-        
-        let camera = Camera2D {
-            // I have no idea why the zoom is this way lmao
-            zoom: vec2(1. / GAME_SIZE_X as f32 * 2., 1. / GAME_SIZE_Y as f32 * 2.),
-            target: vec2(
-                (GAME_SIZE_X as f32 * 0.5f32).floor(),
-                (GAME_SIZE_Y as f32 * 0.5f32).floor(),
-            ),
-            render_target: Some(game_render_target.clone()),
-            ..Default::default()
-        };
-        
         santa.handle_input(&mut world);
-        
+
+        camera.render_target = Some(render_target(GAME_SIZE_X, GAME_SIZE_Y));
+
         set_camera(&camera);
         
-        clear_background(BLACK);
-        game_render_target.texture.set_filter(FilterMode::Nearest);
-
         // DRAW!        
         santa.draw(&world);
         let _nearest_obstacle = game.update(&mut world); //TODO can use that to detect colision
         game.draw();
-        
+
         set_default_camera();
-        
-        // calculate game view size based on window size
-        let game_diff_w = screen_width() / GAME_SIZE_X as f32;
-        let game_diff_h = screen_height() / GAME_SIZE_Y as f32;
-        let aspect_diff = game_diff_w.min(game_diff_h);
-        
-        let scaled_game_size_w = GAME_SIZE_X as f32 * aspect_diff;
-        let scaled_game_size_h = GAME_SIZE_Y as f32 * aspect_diff;
-        
-        let width_padding = (screen_width() - scaled_game_size_w) * 0.5f32;
-        let height_padding = (screen_height() - scaled_game_size_h) * 0.5f32;
-        
+        clear_background(BLACK);
+
+        let render_target = camera.render_target.unwrap();
+        render_target.texture.set_filter(FilterMode::Nearest);
+
         // fit inside window
         draw_texture_ex(
-            &game_render_target.texture,
-            width_padding,
-            height_padding,
+            &render_target.texture,
+            0.,
+            0.,
             WHITE,
             DrawTextureParams {
-                dest_size: Some(Vec2::new(scaled_game_size_w, scaled_game_size_h)),
+                dest_size: Some(Vec2::new(WINDOW_WIDTH as f32, WINDOW_HEIGHT as f32)),
+                flip_y: true,
                 ..Default::default()
             },
         );
+
+        // draw ui here
         
 
         next_frame().await
